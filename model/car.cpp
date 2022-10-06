2a2e2e92ad5dd46692e58002a3aae6b3f9fc7b61
Samuel Chamalé <63200593+chamale-rac@users.noreply.github.com>
2022-10-06 08:02:37 -0600

    car class finished

--- model/car.cpp
+++ model/car.cpp
@@ -4,27 +4,50 @@ using namespace std;
 class Car {
     private:
         int id;
-        int sponsorId
-        int pitStops
-        double pitTime
+        int sponsorId;
+        int pitStops;
+        int speed;
 
     public: 
-        int getId();
-        void setId();
-        int getSponsorId();
-        void setSponsorId();
-        int getSponsorId();
-        void setSponsorId();
-        int getSponsorId();
-        void setSponsorId();
+        int getId() {
+            return id;
+        };
 
+        void setId(int id) {            
+            this->id = id; 
+        };
 
+        int getSponsorId() {
+            return sponsorId
+        };
 
-    Car(double id) {
-        id = id;
+        void setSponsorId(int sponsorId) {
+            this->sponsorId = sponsorId; 
+        };
+
+        int getPitStops() {
+            return pitStops
+        };
+
+        void setPitStops(int pitStops){
+            this->pitStops = pitStops; 
+        };
+
+        double getDistance(int globalTime) {
+            return speed * globalTime;
+        };
+        
+        int getSpeed() {
+            return speed;
+        };
+        void setSpeed(int speed) {            
+            this->speed = speed; 
+        };
+        
+    Car(int id, int sponsorId, int pitStops, int speed) {
+        this->id = id;
+        this->sponsorId = sponsorId;
+        this->pitStops = pitStops;
+        this->speed = speed; 
     }
-
-
-
-
 };
