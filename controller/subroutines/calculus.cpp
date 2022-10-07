991acc9aadbb737c254f2e009acd2ef8389f3120
Samuel Chamalé <63200593+chamale-rac@users.noreply.github.com>
2022-10-07 09:08:55 -0600

    final distributor

--- controller/subroutines/calculus.cpp
+++ controller/subroutines/calculus.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 namespace calculus
 {
-    intervalsCreator()
+    int *distributor(int arr[], int m, int n)
     {
+        for (int i = 0; i < m; i++)
+        {
+            arr[i] = n / m;
+            if (i + 1 - m == 0)
+            {
+                arr[i] = n - n / m * (i);
+            }
+        }
+        return arr;
     }
 }
